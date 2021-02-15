6aecd258cc1c08c733a9867e6151181549c8eb3a
ZolaKolaMolaStudios <79012049+ZolaKolaMolaStudios@users.noreply.github.com>
2021-02-15 12:39:10 +0530

    Update HardAI.cs

--- Assets/Scripts/HardAI.cs
+++ Assets/Scripts/HardAI.cs
@@ -1,1 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
+public class HardAI : MonoBehaviour
+{    
+    //Serialize gridspaces
+    public Text[] gridButtons;
+    int Score;
+    String AISide;
+    
+    void Awake()
+    {
+      
+    }
+    
+    void Update()
+    {
+    
+    }
+    
+    void Minimax()
+    {
+    
+    }
+    
+    void WinCondition()
+    {
+      for(int i = 0; i < 3; i++)
+      {   
+          for(int j = 0; j < 3; j++)
+          {
+              if((gridButtons[i + 3*j] == gridButtons[i + 3*j + 1]) &&(gridButtons[i + 3*j + 1] == gridButtons[i + 3*j + 2])) // Take with a grain of salt
+          }
+             
+      }
+    }
+    
+    void GetAiSide()
+    {
+        //Code getting Ai side here
+    }
+}
